14551f6a720d4aaaeaa3e46d5a5f75c12de8507e
madm0nkey <coralfruit@gmail.com>
2017-05-29 01:22:54 +0300

    задачи 1-3 к 24.05

--- homework/lesson-04.py
+++ homework/lesson-04.py
@@ -2,8 +2,23 @@
 print('Задача 1.\n')
 
 
-# def palindrome_check(str):
+def palindrome_check(string):
+    list_a = list(str(string))
+    list_b = list(list_a)
+    list_b.reverse()
+    if list_a == list_b:
+        return True
+    else:
+        return False
 
+num1 = 12521
+num2 = 56268
+str1 = 'ololo'
+str2 = 'hello'
+print('Is \'{num}\' a palindrome? {res}'.format(num=num1, res=palindrome_check(num1)))
+print('Is \'{num}\' a palindrome? {res}'.format(num=num2, res=palindrome_check(num2)))
+print('Is \'{str}\' a palindrome? {res}'.format(str=str1, res=palindrome_check(str1)))
+print('Is \'{str}\' a palindrome? {res}\n'.format(str=str2, res=palindrome_check(str2)))
 
 
 # задача 2
@@ -25,3 +40,20 @@ def point_coordinates_check(x, y):
 print(
     'Точка лежит в четверти', point_coordinates_check(2, -4)
 )
+
+
+# задача 3
+print('Задача 3.\n')
+
+
+def bubble_sort(num_list):
+    l = len(num_list)
+    i = 0
+    while i < l:
+        for j in range(l - 1):
+            if num_list[j] > num_list[j + 1]:
+                num_list[j], num_list[j + 1] = num_list[j + 1], num_list[j]
+        i += 1
+    return num_list
+
+print(bubble_sort([2, 7, 5, 1, 6]))
