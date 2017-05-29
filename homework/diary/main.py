98fc4da8c6ad9b225147b491000597756e930246
Margarita.Buynova <margarita.buynova@billing.ru>
2017-05-29 11:59:20 +0300

    Доработан ежедневник

--- homework/diary/main.py
+++ homework/diary/main.py
@@ -2,6 +2,8 @@
 Модуль "Ежедневник".
 """
 
+import Tasks
+
 
 def select_action():
     diary_message = """Ежедневник. Выберите действие:
@@ -15,27 +17,28 @@ def select_action():
     """
     print(diary_message)
     id_action = input('Введите номер действия: ')
-
-    if id_action == '1':
-        print('Вы выбрали действие "Вывести список задач"\n')
-        select_action()
-    elif id_action == '2':
-        print('Вы выбрали действие "Добавить задачу"\n')
-        select_action()
-    elif id_action == '3':
-        print('Вы выбрали действие "Отредактировать задачу"\n')
-        select_action()
-    elif id_action == '4':
-        print('Вы выбрали действие "Завершить задачу"\n')
-        select_action()
-    elif id_action == '5':
-        print('Вы выбрали действие "Начать задачу сначала"\n')
-        select_action()
-    elif id_action == '6':
-        print('Вы выбрали действие "Выход"\n')
-        select_action()
-    else:
+    if id_action not in ['1', '2', '3', '4', '5', '6']:
         print('Вы ввели неверные данные.\n')
         select_action()
+    else:
+        id_action = int(id_action)
+
+    if id_action == 1:
+        Tasks.show_tasks()
+        select_action()
+    elif id_action == 2:
+        Tasks.add_task()
+        select_action()
+    elif id_action == 3:
+        Tasks.edit_task()
+        select_action()
+    elif id_action == 4:
+        Tasks.close_task()
+        select_action()
+    elif id_action == 5:
+        Tasks.reopen_task()
+        select_action()
+    elif id_action == 6:
+        Tasks.exit_from_diary()
 
 select_action()
